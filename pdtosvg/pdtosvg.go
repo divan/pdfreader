724e1b775481887015a6683aacd9237b38f04a6c
Ivan Daniluk <ivan.daniluk@gmail.com>
2014-12-14 15:17:00 +0200

    GoFmt and import fixes

--- pdtosvg/pdtosvg.go
+++ pdtosvg/pdtosvg.go
@@ -9,34 +9,34 @@
 package main
 
 import (
-  "fmt"
-  "os"
-  "github.com/yob/pdfreader/pdfread"
-  "github.com/yob/pdfreader/strm"
-  "github.com/yob/pdfreader/svg"
+	"fmt"
+	"github.com/divan/pdfreader/pdfread"
+	"github.com/divan/pdfreader/strm"
+	"github.com/divan/pdfreader/svg"
+	"os"
 )
 
 // The program takes a PDF file and converts a page to SVG.
 
 func complain(err string) {
-  fmt.Printf("%susage: pdtosvg foo.pdf [page] >foo.svg\n", err)
-  os.Exit(1)
+	fmt.Printf("%susage: pdtosvg foo.pdf [page] >foo.svg\n", err)
+	os.Exit(1)
 }
 
 func main() {
-  if len(os.Args) == 1 || len(os.Args) > 3 {
-    complain("")
-  }
-  page := 0
-  if len(os.Args) > 2 {
-    page = strm.Int(os.Args[2], 1) - 1
-    if page < 0 {
-      complain("Bad page!\n\n")
-    }
-  }
-  pd := pdfread.Load(os.Args[1])
-  if pd == nil {
-    complain("Could not load pdf file!\n\n")
-  }
-  fmt.Printf("%s", svg.Page(pd, page))
+	if len(os.Args) == 1 || len(os.Args) > 3 {
+		complain("")
+	}
+	page := 0
+	if len(os.Args) > 2 {
+		page = strm.Int(os.Args[2], 1) - 1
+		if page < 0 {
+			complain("Bad page!\n\n")
+		}
+	}
+	pd := pdfread.Load(os.Args[1])
+	if pd == nil {
+		complain("Could not load pdf file!\n\n")
+	}
+	fmt.Printf("%s", svg.Page(pd, page))
 }
